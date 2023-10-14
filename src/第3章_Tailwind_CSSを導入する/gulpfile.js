5869e43d418dd738ebb67dcedc49f9769fa84772
fsubal <fsubal@users.noreply.github.com>
2023-10-14 13:00:53 +0000

    add gulpfile

---
+++ src/第3章_Tailwind_CSSを導入する/gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require("gulp");
+const postcss = require("gulp-postcss");
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+
+gulp.task("tailwindcss:build", () =>
+  gulp
+    .src("../main.css")
+    .pipe(postcss([tailwindcss, autoprefixer]))
+    .pipe(gulp.dest("../../dist/css")),
+);
