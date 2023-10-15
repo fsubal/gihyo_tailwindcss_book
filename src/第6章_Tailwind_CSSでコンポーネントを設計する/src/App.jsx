2a1a1ef1bfe9f4214e747dc3803cf74d62664cdf
fsubal <fsubal@users.noreply.github.com>
2023-10-15 12:49:39 +0000

    tweak for vite project structure

--- src/第6章_Tailwind_CSSでコンポーネントを設計する/src/App.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1 className="text-2xl font-bold">Hello world!</h1>;
-}
-
-export default App;
---
+++ src/第6章_Tailwind_CSSでコンポーネントを設計する/src/App.tsx
@@ -0,0 +1,12 @@
+import { Card } from "./components/ui/card";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Hello world!</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
