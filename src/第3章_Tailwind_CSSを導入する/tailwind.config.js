597668b12c894d6a97f0f24c3a34cc7b619c44f0
fsubal <fsubal@users.noreply.github.com>
2023-10-15 03:08:30 +0000

    fix

--- src/第3章_Tailwind_CSSを導入する/tailwind.config.js
+++ src/第3章_Tailwind_CSSを導入する/tailwind.config.js
@@ -1,12 +1,11 @@
+const path = require("path");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: {
-    // NOTICE: ここでrelative: trueを指定しているのは、
-    // プロジェクトルートにないtailwind.config.jsを使っているからです（説明の都合上フォルダを章ごとに分けております）
-    // tailwind.config.jsをふつうにプロジェクトルートに置いてるケースでは特に真似する必要はありません
-    relative: true,
-    files: ["./srca/index.html"],
-  },
+  // NOTICE: ここでわざわざ絶対パスを指定しているのは、
+  // プロジェクトルートにないtailwind.config.jsを使っているからです（説明の都合上フォルダを章ごとに分けております）
+  // tailwind.config.jsをふつうにプロジェクトルートに置いてるケースでは特に真似する必要はありません
+  content: [path.resolve(__dirname, "src", "index.html")],
   theme: {
     extend: {},
   },
