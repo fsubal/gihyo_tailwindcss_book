de6281fda2eec097885377e6228d82f9dbf021ad
fsubal <fsubal@users.noreply.github.com>
2023-10-15 01:21:29 +0000

    コメント追加

--- src/第7章_Tailwind_CSSをカスタマイズする/7_3_JavaScript_APIを利用する/jest/jest.config.js
+++ src/第7章_Tailwind_CSSをカスタマイズする/7_3_JavaScript_APIを利用する/jest/jest.config.js
@@ -5,6 +5,8 @@ module.exports = {
     // babelの設定はこのリポジトリの他の部分にとっては不要なものであり、
     // あくまで「src/第7章_Tailwind_CSSをカスタマイズする/7_3_JavaScript_APIを利用する/jest」の中だけで利用したいという理由から
     // .babelrc相当の内容をjest.config.jsで書いているだけです
+    // ふつうにjest+babelを組み合わせて利用する場合は公式ドキュメントを見てください
+    // @see https://jestjs.io/ja/docs/getting-started
     "\\.js$": [
       "babel-jest",
       { presets: [["@babel/preset-env", { targets: { node: "current" } }]] },
