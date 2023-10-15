4d868f4d6a2332035194fa91a3ce8aa4d5f4305c
fsubal <fsubal@users.noreply.github.com>
2023-10-15 06:44:49 +0000

    use npm workspace

--- src/第8章_Tailwind_CSSを既存のプロジェクトに導入する/run.sh
+++ src/第8章_Tailwind_CSSを既存のプロジェクトに導入する/run.sh
@@ -1,8 +1,8 @@
-npx sass src/第8章_Tailwind_CSSを既存のプロジェクトに導入する/sass/ok.scss
-! npx sass src/第8章_Tailwind_CSSを既存のプロジェクトに導入する/sass/ng.scss # コンパイルエラーになって欲しい
+npx sass sass/ok.scss
+! npx sass sass/ng.scss # コンパイルエラーになって欲しい
 
-npx lessc src/第8章_Tailwind_CSSを既存のプロジェクトに導入する/less/ok.less
-npx lessc src/第8章_Tailwind_CSSを既存のプロジェクトに導入する/less/ng.less # 意図しない出力になるが、コンパイルエラーにはならない
+npx lessc less/ok.less
+npx lessc less/ng.less # 意図しない出力になるが、コンパイルエラーにはならない
 
-npx stylus src/第8章_Tailwind_CSSを既存のプロジェクトに導入する/stylus/ok.styl --print
-npx stylus src/第8章_Tailwind_CSSを既存のプロジェクトに導入する/stylus/ng.styl --print # 意図しない出力になるが、コンパイルエラーにはならない
+npx stylus stylus/ok.styl --print
+npx stylus stylus/ng.styl --print # 意図しない出力になるが、コンパイルエラーにはならない
