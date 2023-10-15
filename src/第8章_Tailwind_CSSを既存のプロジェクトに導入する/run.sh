5dbfe24b73c710401718a4cdeef632e16b3d7ec2
fsubal <fsubal@users.noreply.github.com>
2023-10-15 05:54:45 +0000

    add chapter8

---
+++ src/第8章_Tailwind_CSSを既存のプロジェクトに導入する/run.sh
@@ -0,0 +1,8 @@
+npx sass src/第8章_Tailwind_CSSを既存のプロジェクトに導入する/sass/ok.scss
+! npx sass src/第8章_Tailwind_CSSを既存のプロジェクトに導入する/sass/ng.scss
+
+npx lessc src/第8章_Tailwind_CSSを既存のプロジェクトに導入する/less/ok.less
+! npx lessc src/第8章_Tailwind_CSSを既存のプロジェクトに導入する/less/ng.less
+
+npx stylus src/第8章_Tailwind_CSSを既存のプロジェクトに導入する/stylus/ok.styl --print
+! npx stylus src/第8章_Tailwind_CSSを既存のプロジェクトに導入する/stylus/ng.styl --print
