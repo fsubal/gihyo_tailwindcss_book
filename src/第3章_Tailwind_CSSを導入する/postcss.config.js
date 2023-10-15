f18bf707c5f761979371bae7d12e1b15f66be453
fsubal <fsubal@users.noreply.github.com>
2023-10-15 05:32:18 +0000

    fix config path

--- src/第3章_Tailwind_CSSを導入する/postcss.config.js
+++ src/第3章_Tailwind_CSSを導入する/postcss.config.js
@@ -1,5 +1,7 @@
+const path = require("path");
+
 module.exports = {
   plugins: {
-    tailwindcss: { config: require("./tailwind.config.js") },
+    tailwindcss: { config: path.join(__dirname, "./tailwind.config.js") },
   },
 };
