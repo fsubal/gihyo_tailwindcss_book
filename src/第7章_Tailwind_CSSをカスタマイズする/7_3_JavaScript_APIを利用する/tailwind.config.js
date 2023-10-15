fbebc6bf58abd7a5421db812c679828d549002a2
fsubal <fsubal@users.noreply.github.com>
2023-10-15 07:22:21 +0000

    節はworkspaceのルートではない

--- src/第7章_Tailwind_CSSをカスタマイズする/7_3_JavaScript_APIを利用する/tailwind.config.js
+++ src/第7章_Tailwind_CSSをカスタマイズする/7_3_JavaScript_APIを利用する/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: {
+    // NOTICE: ここでrelative: trueを指定しているのは、
+    // プロジェクトルートにないtailwind.config.jsを使っているからです（説明の都合上フォルダを節ごとに分けております）
+    // tailwind.config.jsをふつうにプロジェクトルートに置いてるケースでは特に真似する必要はありません
+    relative: true,
+    files: ["./index.html"],
+  },
   theme: {
     extend: {},
   },
--- src/第7章_Tailwind_CSSをカスタマイズする/7_4_独自のプラグインを作成する/tailwind.config.js
+++ src/第7章_Tailwind_CSSをカスタマイズする/7_4_独自のプラグインを作成する/tailwind.config.js
@@ -12,7 +12,13 @@ const myColorTheme = require("./theme/myColorTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: {
+    // NOTICE: ここでrelative: trueを指定しているのは、
+    // プロジェクトルートにないtailwind.config.jsを使っているからです（説明の都合上フォルダを章ごとに分けております）
+    // tailwind.config.jsをふつうにプロジェクトルートに置いてるケースでは特に真似する必要はありません
+    relative: true,
+    files: ["./index.html"],
+  },
   theme: {
     colors: generateVarNames(myColorTheme.light),
     extend: {},
