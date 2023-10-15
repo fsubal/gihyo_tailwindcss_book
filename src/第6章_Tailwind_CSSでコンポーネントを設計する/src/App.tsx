12819be63f7361cade0e0b871f37f8bdd9164d9c
fsubal <fsubal@users.noreply.github.com>
2023-10-15 12:59:29 +0000

    use shadcn-ui

--- src/第6章_Tailwind_CSSでコンポーネントを設計する/src/App.tsx
+++ src/第6章_Tailwind_CSSでコンポーネントを設計する/src/App.tsx
@@ -1,10 +1,30 @@
-import { Card } from "./components/ui/card";
+import { Button } from "./components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./components/ui/card";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Hello world!</h1>
-      <Card />
+    <div className="py-6 px-8">
+      <h1 className="text-2xl font-bold">Using shadcn-ui</h1>
+      <Card className="mt-2">
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+          <Button>Push me</Button>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
